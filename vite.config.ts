ee7d54310ccbf021fd880089169641d1f7420bec
Angelo Romasanta <angelokenneth.romasanta@esade.edu>
2025-03-19 18:58:41 +0100

    Try vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import Pages from "vite-plugin-pages";
-import getRepoName from "git-repo-name";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => {
-    const isGitHubPages = mode === 'github-pages'
-    const base = isGitHubPages ? `/${getRepoName.sync()}/` : '/'
-  
-    return {
-      plugins: [react(), Pages()],
-      base: base,
-      build: {
-        outDir: isGitHubPages ? 'dist-github' : 'dist'
-      },
-      resolve: {
-        alias: {
-          "@": path.resolve(__dirname, "./src"),
-        },
-      },
-    }
-  })
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
