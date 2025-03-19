bd298742c638aaa837306cae30326c5054b9fbf0
Angelo Romasanta <angelokenneth.romasanta@esade.edu>
2025-03-19 18:36:07 +0100

    Deploy vercel

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
